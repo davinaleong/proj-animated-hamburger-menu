3f816327033b00d518e6c1f55763b950db1f3e5e
Davina Leong <davina.leong.sy@gmail.com>
2022-12-01 10:35:54 +0800

    Fixed animation toggling for button two

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,27 @@
+const dataStateAttr = `data-state`
 const ariaExpandedAttr = `aria-expanded`
 
-const btnThreeEl = document.querySelector(`[data-element="btn-three"]`)
+// const btnThreeEl = document.querySelector(`[data-element="btn-three"]`)
 
-btnThreeEl.addEventListener(`click`, (e) => {
-  const expandedAttr = btnThreeEl.getAttribute(ariaExpandedAttr)
-  if (expandedAttr && expandedAttr === `true`) {
-    btnThreeEl.setAttribute(ariaExpandedAttr, `false`)
-  } else {
-    btnThreeEl.setAttribute(ariaExpandedAttr, `true`)
-  }
+// btnThreeEl.addEventListener(`click`, (e) => {
+//   const expandedAttr = btnThreeEl.getAttribute(ariaExpandedAttr)
+//   if (expandedAttr && expandedAttr === `true`) {
+//     btnThreeEl.setAttribute(ariaExpandedAttr, `false`)
+//   } else {
+//     btnThreeEl.setAttribute(ariaExpandedAttr, `true`)
+//   }
+// })
+
+const btnEls = document.querySelectorAll(`[data-element="btn"]`)
+btnEls.forEach((btn) => {
+  btn.addEventListener(`click`, (e) => {
+    const currentState = btn.getAttribute(dataStateAttr)
+    if (!currentState || currentState === "closed") {
+      btn.setAttribute("data-state", "opened")
+      btn.setAttribute("aria-expanded", "true")
+    } else {
+      btn.setAttribute("data-state", "closed")
+      btn.setAttribute("aria-expanded", "false")
+    }
+  })
 })
